bf51c186feacc176d3ffc978a11c733827ffa382
eluxdesign <info@elux.design>
2020-10-15 14:50:56 +0200

    Adding Vet list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/design/elux/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/design/elux/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package design.elux.petclinic.controllers;
 
+import design.elux.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
