8d70026e49a7387cc8daa0496d027e2a96fc4dce
eluxdesign <info@elux.design>
2020-11-06 10:04:28 +0100

    Add Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/design/elux/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,33 @@
+package design.elux.petclinic.services;
+
+import design.elux.petclinic.model.Speciality;
+
+import java.util.Set;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+    @Override
+    default Set<Speciality> findAll() {
+        return null;
+    }
+
+    @Override
+    default Speciality findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    default Speciality save(Speciality object) {
+        return null;
+    }
+
+    @Override
+    default void delete(Speciality object) {
+
+    }
+
+    @Override
+    default void deleteById(Long aLong) {
+
+    }
+}
